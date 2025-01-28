1b2447c3303c1fe5389119ca36bbe5f50333683b
Devin Droddy <devin.droddy@gmail.com>
2025-01-28 13:45:07 -0500

    setup workspace
    we're on crane now :3

--- flake.nix
+++ flake.nix
@@ -1,22 +1,132 @@
 {
+  description = "Build a cargo project";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
+    self,
     nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
     rust-overlay,
     ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [(import rust-overlay)];
-    };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [(pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))];
-    };
-  };
+      pkgs = import nixpkgs {inherit system overlays;};
+
+      inherit (pkgs) lib;
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default));
+      src = craneLib.cleanCargoSource ./.;
+
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+      };
+
+      craneLibLLvmTools =
+        (crane.mkLib pkgs).overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
+
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      my-crate = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+          postInstall = ''
+            mkdir $out/lua
+            mv $out/lib/libneoshare.so $out/lua/neoshare.so
+            rm -r $out/lib
+          '';
+        });
+    in {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        inherit my-crate;
+
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        my-crate-clippy = craneLib.cargoClippy (commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+        my-crate-doc = craneLib.cargoDoc (commonArgs
+          // {
+            inherit cargoArtifacts;
+          });
+
+        # Check formatting
+        my-crate-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        my-crate-toml-fmt = craneLib.taploFmt {
+          src = pkgs.lib.sources.sourceFilesBySuffices src [".toml"];
+          # taplo arguments can be further customized below as needed
+          # taploExtraArgs = "--config ./taplo.toml";
+        };
+
+        # Run tests with cargo-nextest
+        # Consider setting `doCheck = false` on `my-crate` if you do not want
+        # the tests to run twice
+        my-crate-nextest = craneLib.cargoNextest (commonArgs
+          // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+            cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+          });
+      };
+
+      packages =
+        {
+          default = my-crate;
+        }
+        // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
+        };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = my-crate;
+      };
+
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+      };
+    });
 }
